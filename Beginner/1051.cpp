3b7205d37ac67eb0743494303044c96d1fc8ec76
Tanjila Akter Shamima <tanjila.akter@bjitgroup.com>
2022-07-02 20:03:42 +0600

    some problem solved

---
+++ Beginner/1051.cpp
@@ -0,0 +1,21 @@
+// https://www.beecrowd.com.br/judge/en/problems/view/1051
+// Tanjila Shamima
+// Software Engineer at BJIT Group
+// uri online problem solved by Tanjila Shamima
+// personal website: https://tanjila-shamima.web.app/
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+    double number;
+    cin >> number;
+
+    
+
+    return 0;
+}
