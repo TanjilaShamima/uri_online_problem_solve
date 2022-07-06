82396b38d8b2f9e6537cea8e163e454c55b20857
Tanjila Akter Shamima <tanjila.akter@bjitgroup.com>
2022-07-06 14:18:22 +0600

    6 july 2022

--- Beginner/1051.cpp
+++ Beginner/1051.cpp
@@ -10,12 +10,28 @@
 #include <iomanip>
 using namespace std;
 
-int main() {
+int main()
+{
 
     double number;
     cin >> number;
 
-    
+    if (number <= 2000)
+    {
+        cout << "Isento" << endl;
+    }
+    else if (number <= 3000)
+    {
+        cout << "R$ " << fixed << setprecision(2) << (number - 2000) * 0.08 << endl;
+    }
+    else if (number <= 4500)
+    {
+        cout << "R$ " << fixed << setprecision(2) << (number - 3000) * 0.18 + (1000 * 0.08) << endl;
+    }
+    else
+    {
+        cout << "R$ " << fixed << setprecision(2) << (number - 4500) * 0.28 + (1500 * 0.18) + (1000 * 0.08) << endl;
+    }
 
     return 0;
 }
